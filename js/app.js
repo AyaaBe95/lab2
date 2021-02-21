86d1670e1fde0bcf72e8d2fd79e5cc60cf4ef9c0
Aya berjawi <ayabarjawi@gmail.com>
2021-02-21 20:05:57 +0200

    lab3

--- js/app.js
+++ js/app.js
@@ -1,125 +1,122 @@
 'use strict'
 
+
 $(document).ready(function () {
 
-    var allImages = [];
-    let filter = $('#filter')
+    let imgGallary = []
+
     let main = $('main')
-    let photoTemplate = $('.photo-template')
-    photoTemplate.css({ "display": "none" })
+    let filter = $('#filter')
+    let keywords = []
+    let sortBy = $('#sort')
+
+    let page1 = $('#page1')
+    let page2 = $('#page2')
 
 
 
-    // constructor
-    function Gallery(item) {
-
-        this.title = item.title;
-        this.image_url = item.image_url;
-        this.desc = item.description;
-        this.keyword = item.keyword;
-        this.horns = item.horns
-
-        allImages.push(this)
-
-
-    }
-
-    //ajax
-
-    function pageNum (){
-
+    // Load Json data
+    function getPageDate(page) {
         $.ajax({
-            url: "page-1.json",
+            url: page,
             type: "GET"
         }).then(function (data) {
             data.forEach(item => {
-                let photo = new Gallery(item)
-                photo.render()
-                photo.renderSelct()
-    
-            });
-    
+                new Gallary(item.image_url, item.title, item.description, item.keyword, item.horns)
+            })
+        }).done(() => {
+            Gallary.prototype.renderer(imgGallary)
+            Gallary.prototype.addFliters(keywords)
         })
-
-
-    }
-    
-
-   
-
-    // render img
-    Gallery.prototype.render = function () {
-
-        let dataClone = photoTemplate.clone();
-        dataClone.removeClass('photo-template')
-        dataClone.find('h2').text(this.title);
-        dataClone.find('img').attr("src", this.image_url);
-        dataClone.find('p').text(this.desc);
-        dataClone.css({ "display": "block" })
-        main.append(dataClone);
-
-
     }
 
+    // Gallary constructor
+    function Gallary(image_url, title, description, keyword, horns) {
+        this.image_url = image_url;
+        this.title = title;
+        this.description = description;
+        this.keyword = keyword;
+        this.horns = horns;
+        imgGallary.push(this)
+    }
 
-    // render keywords
-    Gallery.prototype.renderSelct = function () {
+    // Gallary items renderer
+    Gallary.prototype.renderer = function (renderAfterFilter) {
+        let itemsHolder = imgGallary;
 
-        if (allImages.includes(this.keyword) == false) {
-            $('#filter').append(`<option value =${this.keyword}>${this.keyword}</option>`);
-            allImages.push(this.keyword);
-            console.log(allImages)
-
-
+        if (renderAfterFilter != "null" || renderAfterFilter != "undefined") {
+            itemsHolder = renderAfterFilter
+            Gallary.prototype.sorter(renderAfterFilter)
+        }
+        else {
+            Gallary.prototype.sorter(itemsHolder)
         }
 
-
+        itemsHolder.forEach(item => {
+            let photo = $('#template').html();
+            let mustache = Mustache.render(photo, item)
+            main.append(mustache);
+            if (keywords.includes(item.keyword) == false)
+                keywords.push(item.keyword)
+        });
     }
 
-    // filter data
+    // Gallary fillters
+    Gallary.prototype.addFliters = function (data) {
+        filter.append(`<option value='default'selected>default</option>`)
+        data.forEach(item => {
+            filter.append(`<option value='${item}'>${item}</option>`)
+        });
+    }
 
-    filter.change(() => {
+    // Gallary sort
+    Gallary.prototype.sorter = function (sorter) {
+        console.log(sortBy.val())
+        if (sortBy.val() == "title")
+            sorter.sort(function (a, b) {
+                if (a.title > b.title) return 1;
+                if (a.title < b.title) return -1;
+                return 0;
+            })
+        else if (sortBy.val() == "horns") {
+            console.log(sortBy.val())
+            sorter.sort(function (a, b) {
+                if (a.horns > b.horns) return 1;
+                if (a.horns < b.horns) return -1;
+                return 0;
+            })
+        }
+    }
 
-        main.empty()
+    Gallary.prototype.fiterAndSort = function () {
 
+        main.empty();
         if (filter.val() == "default") {
-            allImages.forEach(item => {
-                let dataClone = photoTemplate.clone();
-                dataClone.removeClass('photo-template')
-                dataClone.find('h2').text(item.title);
-                dataClone.find('img').attr("src", item.image_url);
-                dataClone.find('p').text(item.desc);
-                dataClone.css({ "display": "block" })
-                main.append(dataClone);
-
-
-
-            })
+            Gallary.prototype.renderer(imgGallary)
         } else {
-
-            allImages.forEach(item => {
+            let renderAfterFilter = []
+            imgGallary.forEach(item => {
                 if (filter.val() == item.keyword) {
-                    let dataClone = photoTemplate.clone();
-                    dataClone.removeClass('photo-template')
-                    dataClone.find('h2').text(item.title);
-                    dataClone.find('img').attr("src", item.image_url);
-                    dataClone.find('p').text(item.desc);
-                    dataClone.css({ "display": "block" })
-
-                    main.append(dataClone);
-
+                    renderAfterFilter.push(item)
                 }
-
             })
-
-
+            Gallary.prototype.renderer(renderAfterFilter)
         }
-    })
+    }
 
-})
+    function pageLoader(page) {
+        main.empty()
+        filter.empty()
+        imgGallary = []
+        keywords = []
+        getPageDate(page.target.value)
+    }
 
 
+    getPageDate(page1.val())
 
-
-
-
+    page1.click(pageLoader)
+    page2.click(pageLoader)
+    filter.on('change', Gallary.prototype.fiterAndSort)
+    sortBy.on('change', Gallary.prototype.fiterAndSort)
+});
